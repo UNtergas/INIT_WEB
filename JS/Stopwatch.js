57354d5824a371a1fe338526947fd635664a6452
UNtergas <mycomputerer135@gmail.com>
2023-02-03 00:30:06 +0100

    Stopwatch js

--- JS/Stopwatch.js
+++ JS/Stopwatch.js
@@ -1,49 +1,58 @@
 class Stopwatch {
-    /**
-     * 
-     * @param {html item id} my_display_id 
-     * @param {ms} delay 
-     */
-
-    constructor(my_display_id, delay = 100) {
-        this.isPause = true;
+    constructor(id, delay = 100) { //Delay in ms
+        this.state = "paused";
         this.delay = delay;
-        this.display = document.getElementById(my_display_id);
-        this.interval = null;
+        this.display = document.getElementById(id);
         this.value = 0;
     }
+
     formatTime(ms) {
-        let hours = Math.floor(ms / 3600000);
-        let minutes = Math.floor((ms - (hours * 3600000)) / 60000);
-        let seconds = Math.floor((ms - (hours * 3600000) - (minutes * 60000)) / 1000);
-        let ds = Math.floor((ms - (hours * 3600000) - (minutes * 60000) - (seconds * 1000)) / 100);
+        var hours = Math.floor(ms / 3600000);
+        var minutes = Math.floor((ms - (hours * 3600000)) / 60000);
+        var seconds = Math.floor((ms - (hours * 3600000) - (minutes * 60000)) / 1000);
+        var ds = Math.floor((ms - (hours * 3600000) - (minutes * 60000) - (seconds * 1000)) / 100);
 
         if (hours < 10) { hours = "0" + hours; }
         if (minutes < 10) { minutes = "0" + minutes; }
         if (seconds < 10) { seconds = "0" + seconds; }
         return hours + ':' + minutes + ':' + seconds + '.' + ds;
     }
+
     update() {
-        if (this.isPause == false) {
+        if (this.state == "running") {
             this.value += this.delay;
         }
-        this.display.innerHTML = this.formatTime(this.value);
-    }
-    start() {
-        if (this.isPause == true) {
-            this.isPause == false;
-            this.interval = setInterval(this.update, this.delay);
+        var retunring_value = this.formatTime(this.value);
+        if (retunring_value != null) {
+            this.display.innerHTML = retunring_value;
         }
     }
-    stop() {
-        if (this.isPause == false) {
-            this.isPause == true;
-            clearInterval(this.interval);
-            this.interval = null
-        }
-    }
-    reset() {
 
+    start() {
+        if (this.state == "paused") {
+            this.state = "running";
+            if (!this.interval) {
+
+                this.interval = setInterval(function () { stopwatch.update(); }, this.delay);
+            }
+        }
+    }
+
+    stop() {
+        if (this.state == "running") {
+            this.state = "paused";
+            if (this.interval) {
+                clearInterval(this.interval);
+                this.interval = null;
+                alert("Time stopped at " + stopwatch.formatTime(this.value));
+            }
+        }
+    }
+
+    reset() {
+        this.stop();
+        this.value = 0;
+        this.update();
     }
 }
 
@@ -53,8 +62,9 @@ function mainFunction() {
     stop_button = document.getElementById("stop");
     reset_button = document.getElementById("reset");
     start_button = document.getElementById("start");
-    stop_button.addEventListener(() => { stopwatch.stop() });
-    reset_button.addEventListener(() => { stopwatch.re })
+    stop_button.addEventListener("click", () => { stopwatch.stop() });
+    reset_button.addEventListener("click", () => { stopwatch.reset() })
+    start_button.addEventListener("click", () => { stopwatch.start() });
 }
 
-mainFunction();
+window.onload = mainFunction();
